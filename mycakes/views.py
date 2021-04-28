d01f5a217a883b32e565cc35411fa3ec90f8ff5f
vijaya nigam <vijaya.nigam19@gmail.com>
2021-04-28 17:51:59 +0530

    gitignore added

--- mycakes/views.py
+++ mycakes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import render
 from rest_framework import status
 from rest_framework.response import Response
-from mycakes.serializers import  AddCakeSerializer
+from mycakes.serializers import AddCakeSerializer
 from rest_framework.decorators import api_view
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
